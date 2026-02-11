3f48ae3bdd0a0620512c75869ebc3a295920c75a
Karina Kokoshko <karinakokoshko@gmail.com>
2026-02-11 10:59:03 +0200

    Add MovieSession model and constructor

--- CinemaManager.Models/Entities/MovieSession.cs
+++ CinemaManager.Models/Entities/MovieSession.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CinemaManager.Models.Enums;
 
 namespace CinemaManager.Models.Entities
 {
-    internal class MovieSession
+    public class MovieSession
     {
+        public Guid Id { get; }
+        public Guid CinemaHallId { get; }
+        public string MovieTitle { get; set; }
+        public Genre Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public DateTime StartTime { get; set; }
+        public int DurationMinutes { get; set; }
+
+        public MovieSession(
+            Guid id,
+            Guid cinemaHallId,
+            string movieTitle,
+            Genre genre,
+            int releaseYear,
+            DateTime startTime,
+            int durationMinutes)
+        {
+            Id = id;
+            CinemaHallId = cinemaHallId;
+            MovieTitle = movieTitle;
+            Genre = genre;
+            ReleaseYear = releaseYear;
+            StartTime = startTime;
+            DurationMinutes = durationMinutes;
+        }
     }
 }
