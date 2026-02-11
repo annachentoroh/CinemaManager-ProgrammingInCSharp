3a8ec8f57bd0bd0a57bfa402e94b8fc497080cd2
annachentoroh <annachentoroh@gmail.com>
2026-02-11 20:19:35 +0200

    Add UI logic for displaying movie sessions with cinema halls and sessions

--- CinemaManager.UI/CinemaHallUI.cs
+++ CinemaManager.UI/CinemaHallUI.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using CinemaManager.Models.Entities;
+using CinemaManager.Models.Enums;
 
 namespace CinemaManager.UI
 {
-    internal class CinemaHallUI
+    public class CinemaHallModel
     {
+        private readonly CinemaHall _entity;
+        public List<MovieSessionUI> Sessions { get; set; } = new List<MovieSessionUI>();
+
+        public CinemaHallModel(CinemaHall entity)
+        {
+            _entity = entity;
+        }
+
+        public Guid Id => _entity.Id;
+        public string Name
+        {
+            get => _entity.Name;
+            set => _entity.Name = value;
+        }
+        public int SeatsCount => _entity.SeatsCount;
+        public HallType HallType => _entity.HallType;
+        public double TotalDurationMinutes => Sessions.Sum(s => s.DurationMinutes);
+
+        public override string ToString()
+        {
+            return $"Зал: {Name} [{HallType}] - Місць: {SeatsCount}";
+        }
     }
 }
--- CinemaManager.UI/MovieSessionUI.cs
+++ CinemaManager.UI/MovieSessionUI.cs
@@ -1,10 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using CinemaManager.Models.Entities;
+using CinemaManager.Models.Enums;
 
 namespace CinemaManager.UI
 {
-    internal class MovieSessionUI
+    public class MovieSessionUI
     {
+        private readonly MovieSession _entity;
+
+        public MovieSessionUI(MovieSession entity)
+        {
+            _entity = entity;
+        }
+
+        public Guid Id => _entity.Id;
+        public Guid CinemaHallId => _entity.CinemaHallId;
+        public string MovieTitle
+        {
+            get => _entity.MovieTitle;
+            set => _entity.MovieTitle = value;
+        }
+        public Genre Genre => _entity.Genre;
+        public DateTime StartTime => _entity.StartTime;
+        public int DurationMinutes => _entity.DurationMinutes;
+        public DateTime EndTime => StartTime.AddMinutes(DurationMinutes);
+
+        public override string ToString()
+        {
+            return $"{MovieTitle} ({Genre}) | {StartTime:HH:mm} - {EndTime:HH:mm}";
+        }
     }
 }
