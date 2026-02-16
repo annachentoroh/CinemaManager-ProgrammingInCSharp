288341d32362b0337bfba08ec46ac59ec224cb9b
Karina Kokoshko <karinakokoshko@gmail.com>
2026-02-16 17:07:26 +0200

    Add CinemaRepo logic

--- CinemaManager.DB/CinemaRepo.cs
+++ CinemaManager.DB/CinemaRepo.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using CinemaManager.Models.Entities;
+using CinemaManager.UI;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace CinemaManager.DB
 {
-    internal class CinemaRepo
+    public class CinemaRepo
     {
+        public List<CinemaHallUI> GetAllHalls()
+        {
+            return Database.CinemaHalls
+                .Select(h => new CinemaHallUI(h))
+                .ToList();
+        }
+        public List<MovieSessionUI> GetSessionsByHallId(Guid hallId)
+        {
+            return Database.MovieSessions
+                .Where(s => s.CinemaHallId == hallId)
+                .Select(s => new MovieSessionUI(s))
+                .ToList();
+        }
     }
 }
