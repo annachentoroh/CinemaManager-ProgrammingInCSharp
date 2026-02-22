62337442fe709790053374ac19f5fa29041d81db
annachentoroh <annachentoroh@gmail.com>
2026-02-22 23:02:00 +0200

    Initialize .NET MAUI project structure

---
+++ CinemaManager.MAUI/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CinemaManager.MAUI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        Routing.RegisterRoute(nameof(HallDetailsPage), typeof(HallDetailsPage));
+        Routing.RegisterRoute(nameof(SessionDetailsPage), typeof(SessionDetailsPage));
+    }
+}
