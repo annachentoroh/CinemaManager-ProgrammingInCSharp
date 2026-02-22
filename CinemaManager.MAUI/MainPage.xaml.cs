16d8e4e0ebef5cdf4b50e951069d187b123cd09e
annachentoroh <annachentoroh@gmail.com>
2026-02-22 23:04:35 +0200

    Setup MainPage with navigation to hall details

---
+++ CinemaManager.MAUI/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CinemaManager.DB;
+using CinemaManager.UI;
+
+namespace CinemaManager.MAUI;
+
+public partial class MainPage : ContentPage
+{
+    private readonly ICinemaRepo _storageService;
+
+    public MainPage(ICinemaRepo storageService)
+    {
+        InitializeComponent();
+        _storageService = storageService;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        var hallEntities = _storageService.GetAllHalls();
+        var uiHalls = hallEntities.Select(h => new CinemaHallUI(h)).ToList();
+        HallsCollection.ItemsSource = uiHalls;
+    }
+
+    private async void OnHallSelected(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.FirstOrDefault() is CinemaHallUI selectedHall)
+        {
+            var parameters = new Dictionary<string, object> { { "SelectedHall", selectedHall } };
+            await Shell.Current.GoToAsync(nameof(HallDetailsPage), parameters); // Перехід на деталі залу
+            HallsCollection.SelectedItem = null; // Скидання виділення
+        }
+    }
+}
---
+++ CinemaManager.MAUI/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using CinemaManager.DB;
+
+namespace CinemaManager.MAUI;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // Реєстрація сервісів (Dependency Injection)
+        builder.Services.AddSingleton<ICinemaRepo, CinemaRepo>();
+
+        // Реєстрація сторінок
+        builder.Services.AddTransient<MainPage>();
+        builder.Services.AddTransient<HallDetailsPage>();
+        builder.Services.AddTransient<SessionDetailsPage>();
+
+        return builder.Build();
+    }
+}
