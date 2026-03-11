68fdb95b48a4e3073db31f543a25363253ac483f
Karina Kokoshko <karinakokoshko@gmail.com>
2026-03-11 10:57:50 +0200

    Add CinemaManager.Services project to Solution

---
+++ CinemaManager.Services/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CinemaManager.Services
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ CinemaManager.Services/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CinemaManager.Services
+{
+    // All the code in this file is only included on Tizen.
+    public class PlatformClass1
+    {
+    }
+}
